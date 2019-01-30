21e24a60eaf1e742545ee086f713446f88b42b1b
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2019-01-30 10:52:02 -0800

    setup and hello world

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = null
+  }
+
+  render () {
+    return (
+      <div> Hello World</div>
+      )
+  }
+}
 
 
+ReactDOM.render(<App />, document.getElementById('app'));
