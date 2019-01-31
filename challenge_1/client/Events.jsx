2448b97cacc86631a42cc8fd6c33e16d65e05790
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2019-01-30 17:21:38 -0800

    working version without pagination

---
+++ challenge_1/client/Events.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class Events extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if(this.props.data.length === 0) {
+       return null;
+     } else {
+       let eventNodes = this.props.data.map(function(event, index) {
+         let eventSummary = event.description.split('.')
+         return <div key={index}>{eventSummary[0]}</div>;
+       });
+
+      return (
+        <div id="historical-events" className="eventList">
+          <ul className="events">{eventNodes}</ul>
+        </div>
+      );
+     }
+  }
+}
+
+
--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,19 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import Events from './Events.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = null
+    this.state = {
+      query:'',
+      results: [],
+      offset: 0,
+      perPage: 10
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.getSearchedEvents = this.getSearchedEvents.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
+
+  componentDidMount() {
+    //this.getSearchedEvents();
+  }
+
+  handleInputChange () {
+    this.setState({
+      query: this.search.value
+    })
+  }
+
+  getSearchedEvents () {
+    axios.get(`/events?q=${this.state.query}&limit=${this.state.perPage}`)
+      .then((response) => {
+        console.log('response', response)
+        this.setState({
+          results: response.data,
+          pageCount: Math.ceil(response.data.length / 10),
+        }, ()=> {
+        })
+      })
+  }
+
+  handlePageClick (data) {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * this.state.perPage);
+
+    this.setState({ offset: offset }, () => {
+      this.getSearchedEvents();
+    });
+  };
 
   render () {
     return (
-      <div> Hello World</div>
+      <div>
+   
+        <form>
+          <input
+            placeholder="Search for..."
+            ref={input => this.search = input}
+            onChange={this.handleInputChange}
+            
+          />
+          <p>{this.props.query}</p>
+        </form>
+        <button onClick={this.getSearchedEvents}>
+              Search
+          </button>
+        <Events data={this.state.results} />
+      </div>
       )
   }
 }
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
