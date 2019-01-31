c4a92a76d6206762642c2473ad2b60fa1a2cd77f
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2019-01-31 15:17:30 -0800

    basic chart render

--- challenge_2/client/CurrencyView.jsx
+++ challenge_2/client/CurrencyView.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import Chart from '../public/Chart.bundle.js';
+import { Line } from 'react-chartjs-2';
 
+let chartOptions = {
+  labels: [],
+  scales: {
+            yAxes: [{
+                ticks: {
+                    min: 3500,
+                    max: 4000,
+                    stepSize: 50,
+                    showLine: true
+                }
+            }]
+          },
+  fill: false
+};
 
 export default class CurrencyView extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.parsePropData = this.parsePropData.bind(this);
   }
 
+
+
+  parsePropData() {
+    let closingPrices = Object.keys(this.props.history).map((prices)=>{
+                return this.props.history[prices];
+              })
+    //console.log('parser', closingPrices)
+
+    let chartData = {
+      labels: ["Previous 31 Days"],
+      datasets: [{
+        label: "Bitcoin closing prices",
+        borderColor: 'rgb(255, 99, 132)',
+        data: closingPrices,
+        }]      
+    }
+
+    //console.log('chart', chartData)
+    return chartData;
+  }
+
+  
   render() {
 
     if (this.props.history === null || this.props.history === undefined) {
@@ -20,9 +59,12 @@ export default class CurrencyView extends React.Component {
           </div>
 
           <div className="history">
-            Bitcoin closing prices: {Object.keys(this.props.history).map((prices, index)=> {
-              return <div className="closing-price" key={index}> {this.props.history[prices]}</div>
-            })}
+            <Line
+              data={this.parsePropData}
+              options={chartOptions}
+              height={500}
+              width={700}
+            />
           </div>
         </div>
       )
